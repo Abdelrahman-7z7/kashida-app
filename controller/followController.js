f53fcde4324a0ed1c9032ed12ea38683713c21e8
Abdelrahman Reyad <92989840+Abdelrahman-7z7@users.noreply.github.com>
2024-12-17 21:10:21 +0300

    adjusting the fetching or followers and followings to provide the follow-status at each doc to determine the status of the follow-button

--- controller/followController.js
+++ controller/followController.js
@@ -74,14 +74,33 @@ exports.unfollow = catchAsync(async (req, res, next) => {
 //get followers of a user
 exports.getFollowers = catchAsync(async (req, res, next)=> {
     const targetedUser = req.params.userId
+    const currentUser = req.user.id
 
+    //1) fetch followers
     const followers = await Follow.find({following: targetedUser}).setOptions({ populateFollowersOnly: true });
+    
+    //2) collect all followers IDs
+    const followersIds = followers.map(follower => follower.follower._id);
+
+    //3) check if the current user is following any of these followers
+    const followStatus = await Follow.find({follower: currentUser, following: {$in: followersIds}})
+
+    // 4) create a map for follow status
+    const followingsSet = new Set(followStatus.map(follow => follow.following._id.toString()))
+
+    // 5) Map over followers and add follow status
+    const followersWithStatus = followers.map(followerDoc =>({
+        ...followerDoc.toObject(),
+        isFollowing: followingsSet.has(followerDoc.follower._id.toString())
+        
+    }))
+    
 
     //sending the response
     res.status(200).json({
         status:'success',
         data:{
-            followers
+            followersWithStatus
         }
     })
 })
@@ -89,14 +108,32 @@ exports.getFollowers = catchAsync(async (req, res, next)=> {
 //get following of a user
 exports.getFollowings = catchAsync(async (req, res, next)=> {
     const targetedUser = req.params.userId
+    const currentUser = req.user.id
 
+    //1) fetch the followings
     const followings = await Follow.find({follower: targetedUser}).setOptions({populateFollowingsOnly: true})
+    
+    //2) collect all the followingsIds
+    const followingsIds = followings.map(following => following.following._id)
 
+    //3) check if the logged in user is following any of these followings
+    const followStatus = await Follow.find({follower: currentUser, following: {$in: followingsIds}})
+
+    //4) create map of follow status
+    const followingsSet = new Set(followStatus.map(follow => follow.following._id.toString()));
+
+    //5) add follow status to each following
+    const followingsWithStatus = followings.map(followingDoc => ({
+        ...followingDoc.toObject(),
+        isFollowing: followingsSet.has(followingDoc.following._id.toString())
+    }))
+
+    
     //sending the response
     res.status(200).json({
         status:'success',
         data:{
-            followings
+            followingsWithStatus
         }
     })
 })
@@ -105,14 +142,31 @@ exports.getFollowings = catchAsync(async (req, res, next)=> {
 //get my followers
 exports.getMyFollowers = catchAsync(async (req, res, next)=>{
     const currentUser = req.user.id
-
+    
+    //1) fetch my followers
     const followers = await Follow.find({following: currentUser}).setOptions({ populateFollowersOnly: true });
     
+    //2) collect all followersIDs 
+    const followersIds = followers.map(follower => follower.follower._id)
+
+    //3) check if the current user is following any of these followers
+    const followBackStatus = await Follow.find({follower: currentUser, following: {$in: followersIds}})
+
+    //4) create a map of follow-back status 
+    const followingBackSet = new Set(followBackStatus.map(follow => follow.following._id.toString())) 
+
+    //5) add follow-back status to followers
+    const followersWithStatus = followers.map(followerDoc => ({
+        ...followerDoc.toObject(),
+        isFollowing: followingBackSet.has(followerDoc.follower._id.toString())
+    }))
+
+
     //sending the response
     res.status(200).json({
         status: 'success',
         data:{
-            followers
+            followersWithStatus
         }
     })
 })
@@ -121,13 +175,29 @@ exports.getMyFollowers = catchAsync(async (req, res, next)=>{
 //get my followings
 exports.getMyFollowings = catchAsync(async (req, res, next)=>{
     const currentUser = req.user.id
-
+    
     const followings = await Follow.find({follower: currentUser}).setOptions({populateFollowingsOnly: true})
+    
+    // Add the isFollowing field as true to each following
+    const followingsWithStatus = followings.map(follow => ({
+        ...follow.toObject(),
+        isFollowing: true, // Since the current user is following these users
+    }));
 
     res.status(200).json({
         status: 'success',
         data:{
-            followings
+            followingsWithStatus
         }
     })
 })
+
+
+// const followersWithStatus = followings.map(follower => ({
+//     ...follower.toObject(),
+//     isFollowing: follower.checkIfFollowing(req.user.id)
+// }))
+// const followersWithStatus = followers.map(follower => ({
+//     ...follower.toObject(),
+//     isFollowing: follower.checkIfFollowing(req.user.id)
+// }))
